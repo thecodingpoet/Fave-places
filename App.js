c84d6b06c1dd078ee7833074da9aaa80e1c38c4d
Oluwatomi Duyile <thecodingpoet@gmail.com>
2018-07-03 14:41:15 +0100

    style button and input element

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: ''
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style= {styles.placeInput}
+            placeholder= 'An awesome place'
+            value={this.state.placeName} 
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Button title='Add' style= {styles.placeButton} />
+        </View>
       </View>
     );
   }
@@ -16,8 +26,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: '80%'
+  },
+  placeButton: {
+    width: '20%'
+  }
 });
